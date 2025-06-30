1cd1724745cb120e7c6deee3c9ed9f8eef312fcc
AlexisRevol <revol.alexis.lgm@gmail.com>
2025-07-01 00:22:21 +0200

    fix: change css file

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import type { CardData } from '@/types';
 import Card from '@/components/Card';
 import CardForm from '@/components/CardForm';
 import CardSkeleton from '@/components/CardSkeleton';
-import '@/App.css';
+import '@/global.css';
 import html2canvas from 'html2canvas'; 
 
 const initialCardData: CardData = {
